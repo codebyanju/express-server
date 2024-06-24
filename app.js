727c1251011132a77c384d246705af2bffed20ea
Anju S <codebyanju@gmail.com>
2024-06-24 20:08:13 +0530

    added cors headers

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.use(function (req, res, next) {
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+
+  console.log(req.headers);
+  // console.log(req.headers.host);
+
+  next();
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello World!ssss andsk');
+  res.send('Hello World! slash');
+});
+
+app.get('/hello/abc', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
